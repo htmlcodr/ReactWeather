db02c2b6e5db4b04a8f645a9469755ff45893494
ahuber <andreas.huber@thestreet.com>
2017-02-21 10:13:54 -0600

    Added Copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,10 @@ var React = require('react');
 var Examples = React.createClass({
   render: function () {
     return (
-      <h3>Examples!</h3>
+      <div>
+        <h3>Examples!</h3>
+        <p>Welcome to example</p>
+      </div>
     )
   }
 });
